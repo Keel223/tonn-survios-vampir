156decde56758e42ddf2edbdd0cf38f54c9fdba5
Keel223 <sasha.yliseev@gmail.com>
2026-05-02 17:33:39 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Phaser не любит строгий режим React
+  reactStrictMode: false,
+  
+  // МАГИЯ ЗДЕСЬ: Эта настройка говорит Next.js, что '@/' означает корень проекта
+  experimental: {
+    typedRoutes: true,
+  },
 }
 
 module.exports = nextConfig
