97cd096f61cef17bc951770fc16f5a22fd9f622a
Keel223 <sasha.yliseev@gmail.com>
2026-05-02 18:09:36 +0300

    Remove experimental settings from next.config.js
    Removed experimental typed routes configuration.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  
-  // МАГИЯ ЗДЕСЬ: Эта настройка говорит Next.js, что '@/' означает корень проекта
-  experimental: {
-    typedRoutes: true,
-  },
 }
 
 module.exports = nextConfig
