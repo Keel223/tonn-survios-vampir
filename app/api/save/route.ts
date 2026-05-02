4586ded00ff256eab3a215393a7e282ba917a1b3
Keel223 <sasha.yliseev@gmail.com>
2026-05-02 17:44:23 +0300

    Update route.ts

--- app/api/save/route.ts
+++ app/api/save/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { redis } from '@/lib/redis';
+import { redis } from '../../../lib/redis'; // ПРЯМОЙ ПУТЬ
 
 export async function POST(req: Request) {
   try {
